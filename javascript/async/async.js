ae251a5051f764ad92b656720be22c98246dca1c
Rohit Kumar <162014017+Rohitkumarsimar@users.noreply.github.com>
2026-08-23 18:30:00 +0530

    Practice JavaScript async patterns

--- javascript/async/async.js
+++ javascript/async/async.js
@@ -1,67 +1,67 @@
-// console.log("A");
-// const promise = fetch("https://jsonplaceholder.typicode.com/todos/1")
-//   .then((res) => res.json())
-//   .then((json) => console.log(json))
-//   .catch((error) => console.log(error));
+console.log("A");
+const promise = fetch("https://jsonplaceholder.typicode.com/todos/1")
+  .then((res) => res.json())
+  .then((json) => console.log(json))
+  .catch((error) => console.log(error));
 
-// setTimeout(() => {
-//   console.log(promise);
-// }, 2000);
+setTimeout(() => {
+  console.log(promise);
+}, 2000);
 
-// const promise2 = promise.then((resolve) => "hello")
-// .then(value =>console.log(value))
+const promise2 = promise.then((resolve) => "hello")
+.then(value =>console.log(value))
 
-// console.log("C");
+console.log("C");
 
-// console.log(promise instanceof Promise);
+console.log(promise instanceof Promise);
 
 
-// async function test(){
-//     await fetch("https://jsonplaceholder.typicode.com/todos/1")
+async function test(){
+    await fetch("https://jsonplaceholder.typicode.com/todos/1")
    
-//     console.log("A")
-// }
+    console.log("A")
+}
 
-// test();
+test();
 
-// const p = new Promise((resolve,reject)=>{
-//     setTimeout(() => {
-//         resolve("result aa gya")
-//         reject("error aa gya");
-//     }, 2000);
-// }).then(val=>console.log(val)).catch(err=>console.log(err))
+const p = new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        resolve("result aa gya")
+        reject("error aa gya");
+    }, 2000);
+}).then(val=>console.log(val)).catch(err=>console.log(err))
 
-// console.log(p)
-//     setTimeout(()=>{console.log(p)},3000);
+console.log(p)
+    setTimeout(()=>{console.log(p)},3000);
 
 
-//     const arr = [1,2,3,4,5]
-//     const newarr = arr.map((i)=>{console.log("hehehe",i)})
-//     const fil = arr.filter((i)=>i>3)
-//     console.log(fil)
-//     console.log(newarr)
+    const arr = [1,2,3,4,5]
+    const newarr = arr.map((i)=>{console.log("hehehe",i)})
+    const fil = arr.filter((i)=>i>3)
+    console.log(fil)
+    console.log(newarr)
 
-// console.log("A")
-// const process = new Promise ((resolve, reject)=>{
-//     const time = new Date().getTime();
-//     while(time+3000>= new Date().getTime());
-//     console.log("C")
-//     resolve();
-// })
+console.log("A")
+const process = new Promise ((resolve, reject)=>{
+    const time = new Date().getTime();
+    while(time+3000>= new Date().getTime());
+    console.log("C")
+    resolve();
+})
 
-// // process();
-// console.log("B")
+// process();
+console.log("B")
 
-// function foo(){
-//     throw new Error ("OOPS!!")
-// }
-// function bar(){
-//     foo()
-// }
-// function baz(){
-//     baz()
-// }
-// baz();
+function foo(){
+    throw new Error ("OOPS!!")
+}
+function bar(){
+    foo()
+}
+function baz(){
+    baz()
+}
+baz();
 
 setTimeout(()=>{
 console.log("timeout")
