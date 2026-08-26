ca0f18d71e9878f089fd21f0246a2d98741e0ff6
Rohit Kumar <162014017+Rohitkumarsimar@users.noreply.github.com>
2026-08-26 10:06:02 +0530

    event loop

--- javascript/async/async.js
+++ javascript/async/async.js
@@ -16,27 +16,55 @@
 // console.log(promise instanceof Promise);
 
 
-async function test(){
-    await fetch("https://jsonplaceholder.typicode.com/todos/1")
+// async function test(){
+//     await fetch("https://jsonplaceholder.typicode.com/todos/1")
    
-    console.log("A")
-}
+//     console.log("A")
+// }
 
-test();
+// test();
 
-const p = new Promise((resolve,reject)=>{
-    setTimeout(() => {
-        resolve("result aa gya")
-        reject("error aa gya");
-    }, 2000);
-}).then(val=>console.log(val)).catch(err=>console.log(err))
+// const p = new Promise((resolve,reject)=>{
+//     setTimeout(() => {
+//         resolve("result aa gya")
+//         reject("error aa gya");
+//     }, 2000);
+// }).then(val=>console.log(val)).catch(err=>console.log(err))
 
-console.log(p)
-    setTimeout(()=>{console.log(p)},3000);
+// console.log(p)
+//     setTimeout(()=>{console.log(p)},3000);
 
 
-    const arr = [1,2,3,4,5]
-    const newarr = arr.map((i)=>{console.log("hehehe",i)})
-    const fil = arr.filter((i)=>i>3)
-    console.log(fil)
-    console.log(newarr)
+//     const arr = [1,2,3,4,5]
+//     const newarr = arr.map((i)=>{console.log("hehehe",i)})
+//     const fil = arr.filter((i)=>i>3)
+//     console.log(fil)
+//     console.log(newarr)
+
+// console.log("A")
+// const process = new Promise ((resolve, reject)=>{
+//     const time = new Date().getTime();
+//     while(time+3000>= new Date().getTime());
+//     console.log("C")
+//     resolve();
+// })
+
+// // process();
+// console.log("B")
+
+// function foo(){
+//     throw new Error ("OOPS!!")
+// }
+// function bar(){
+//     foo()
+// }
+// function baz(){
+//     baz()
+// }
+// baz();
+
+setTimeout(()=>{
+console.log("timeout")
+},0);
+
+Promise.resolve().then(()=>setTimeout(()=>{console.log("promise")},0));
