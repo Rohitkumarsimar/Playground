aeb645b513a3eefb40faad8c14c8fafd4b276581
Rohit Kumar <162014017+Rohitkumarsimar@users.noreply.github.com>
2026-08-29 21:12:21 +0530

    simple array qestion

--- javascript/array.js
+++ javascript/array.js
@@ -1,7 +1,22 @@
-let arr = [11,22,33,44,55,66,77,88]
+// let arr = [11,22,33,44,55,66,77,88]
 
-let squaredArr = arr.map
-((value)=> value * 2)
+// let squaredArr = arr.map
+// ((value)=> value * 2)
 
-console.log(squaredArr)
-console.log(arr)
+// console.log(squaredArr)
+// console.log(arr)
+
+let array = [1,2,3,4,5]
+
+// make new array, multiples of rest of the element except the one on you are
+let result=[];
+let temp=1;
+for(let i = 0; i<array.length; i++){
+    temp *= array[i]
+}
+console.log(temp)
+
+for(let i = 0; i < array.length; i++){
+    result[i] = temp/array[i]
+}
+console.log(result)
